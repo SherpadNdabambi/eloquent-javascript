f8349044a8db72c8d82ddc3c6fd7ae8502f56395
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-01-04 20:18:41 +0200

    Add Chapter 2 solutions

---
+++ Chapter 2/files/js/looping-a-triangle-1.js
@@ -0,0 +1,5 @@
+for(let i = 1; i <= 7; i++){
+	let word = '';
+    for(let j = 0; j < i; j++) word += '#';
+    console.log(word);
+}
---
+++ Chapter 2/files/js/looping-a-triangle-2.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 7; i++){
+	let word = '';
+	while(word.length <= i) word += '#';
+	console.log(word);
+}
