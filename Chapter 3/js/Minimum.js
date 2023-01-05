f50c303a9ed3af2617aa100588daeb3b063448c8
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-01-05 18:41:32 +0200

    Solve Chapter 3 Minimum Exercise

---
+++ Chapter 3/js/Minimum.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns {number} the minumum of num1 and num2
+ */
+function min(num1, num2) {
+   return (num1 < num2) ? num1 : num2;
+}
