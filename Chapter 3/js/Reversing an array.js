2fa351aad2f24b84f48535db3d9625a1f01c2cf3
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-01-06 19:56:36 +0200

    Solve Chapter 4 Reversing an array exercise

---
+++ Chapter 3/js/Reversing an array.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @param {Array} array
+ * @returns {Array} a new array with the same elements in the reverse order
+ */
+function reverseArray(array) {
+
+   let newArray = [];
+
+   for (let i = array.length - 1; i > -1; i--) {
+      newArray.push(array[i]);
+   }
+
+   return newArray;
+}
+
+/**
+ * reverses the elements of the array given as its argument
+ * @param {Array} array
+ */
+function reverseArrayInPlace(array) {
+
+   let temp;
+   for (let i = 0; i < array.length / 2; i++) {
+      temp = array[i];
+      array[i] = array[array.length - i - 1];
+      array[array.length - i - 1] = temp;
+   }
+}
