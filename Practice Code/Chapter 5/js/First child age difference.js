f9ce6b72e89c8a1b33167dc6aea3c4f88de56e36
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-04-24 20:03:58 +0200

    Add practice code

---
+++ Practice Code/Chapter 5/js/First child age difference.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+
+  let parents = ancestry.filter((candidate) => {
+      return hasChild(candidate);
+    });
+
+  firstChildAgeDifferences = parents.map((parent) => {
+    
+    parent.firstChildAgeDifference = Math.abs(parent.born - findFirstChild(parent).born);
+    return parent;
+  });
+
+  console.log(firstChildAgeDifferences)
+}
+
+function firstChildAgeDifference(parent) {
+	
+	// declare local variables
+	let parentBirthYear = parent.born,
+	    firstChildBirthYear = findFirstChild(parent).born;
+
+	return firstChildBirthYear - parentBirthYear;
+}
+
+function hasChild(candidate) {
+  return findChildren(candidate).length > 0;
+}
