6e7553d830915b7f906ef366fcd6a4a25af90b29
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-04-25 19:56:19 +0200

    Complete Historical life expectancy exercise bonus challenge

--- My Answers to Exercises/Chapter 5/js/Historical life expectancy.js
+++ My Answers to Exercises/Chapter 5/js/Historical life expectancy.js
@@ -1,37 +1,62 @@
 window.onload = () => {
-  
+
     // declare local variables
     let ancestry = JSON.parse(ANCESTRY_FILE),
-        centuries = {},
+        byCentury = groupBy(ancestry, getCentury),
         lifeExpectancy,
         people;
-    
-    ancestry.forEach((ancestor) => {
 
-        // declare local variables
-        let centuryString = `${Math.ceil(ancestor.died / 100)}`;
-
-        // create object to hold century members if it doesn't exist
-        if (!centuries[centuryString]) centuries[centuryString] = [];
-
-        // group ancestor by century
-        centuries[centuryString].push(ancestor);
-    });
-
-    Object.keys(centuries).forEach((key) => {
-
-        people = centuries[key];               // get the people from the century
+    Object.entries(byCentury).forEach((century) => {
 
         // calculate life expectancy
-        lifeExpectancy = average(people.map((person) => {
-          return person.died - person.born;
+        lifeExpectancy = average(century[1].map((person) => {
+            return person.died - person.born;
         }));
 
-        // log output to console
-        console.log(
-          key,
-          "century life expectancy: ",
-          lifeExpectancy
-        );
+        // // log output to console
+        console.log(century[0], "century life expectancy: ", lifeExpectancy);
+
+
+        // console.log(
+        //     key,
+        //     "century life expectancy: ",
+        //     lifeExpectancy
+        // );
     });
 }
+
+/**
+ *
+ * @param {Array} array
+ * @param {Function} f function to compute an element's group
+ * @returns {Object}
+ */
+function groupBy(array, f) {
+
+    // declare local variables
+    let grouped = {};
+
+    // goup elements
+    array.forEach((element) => {
+        let groupName = f(element);
+        if (!grouped[groupName]) grouped[groupName] = [];
+        grouped[groupName].push(element);
+    });
+
+    // return result
+    return grouped;
+}
+
+function groupByCentury(person) {
+
+  // declare local variables
+  let centuryString = `${getCentury(person)}`;
+
+  // create object to hold century members if it doesn't exist
+  if (!group[centuryString]) centuries[centuryString] = [];
+
+  // group person by century
+  group[centuryString].push(person);
+}
+
+function getCentury(person) { return Math.ceil(person.died / 100); }
