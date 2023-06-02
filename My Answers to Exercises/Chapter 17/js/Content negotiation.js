0e2ecf897c4a6c7c40859db2f8ae99e2534818bd
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-06-02 23:55:08 +0200

    Complete content negotiation exercise

---
+++ My Answers to Exercises/Chapter 17/js/Content negotiation.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+/** @type {string} */
+const url = "https://eloquentjavascript.net/author";
+
+makeRequest("text/plain");
+makeRequest("text/html");
+makeRequest("application/json");
+makeRequest("application/rainbow+unicorns");
+
+function makeRequest(mediaType) {
+  /** @type {XMLHttpRequest} */
+  let request = new XMLHttpRequest();
+
+  request.open("GET", url);
+  request.setRequestHeader("Accept", mediaType);
+  request.addEventListener("load", function () {
+    console.log(
+      `${mediaType}:
+${request.responseText}`
+    );
+  });
+  request.send(null);
+}
